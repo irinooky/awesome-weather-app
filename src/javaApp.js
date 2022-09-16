a74e6e8f2c50a0f8c89fef385242f6276737abc1
irinooky <32641154+irinooky@users.noreply.github.com>
2022-09-16 12:15:14 +0200

    improved Js

--- src/javaApp.js
+++ src/javaApp.js
@@ -1,72 +1,85 @@
-let currentHour = document.querySelector("#hour");
-let currentMinutes = document.querySelector("#minutes");
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${minutes}`;
+  }
+  let minutes = date.getMinutes();
 
-let now = new Date();
-let hour = now.getHours();
-let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-if (hour < 10) {
-  hour = `0${minutes}`;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tueday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-let currentDay = document.querySelector("#wed");
-let days = [
-  "Sunday",
-  "Monday",
-  "Tueday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-console.log(day);
-currentDay.innerHTML = `${day}`;
-
-console.log(hour);
-console.log(minutes);
-currentHour.innerHTML = `${hour}:${minutes}`;
-
-let units = "metric";
 
 function displayWeatherCondition(response) {
   console.log(response.data);
-  document.querySelector("#currentCity").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#status").innerHTML = response.data.weather[0].main;
-}
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#currentCity");
+  let statusElement = document.querySelector("#status");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
 
-function search(event) {
-  event.preventDefault();
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  statusElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+let units = "metric";
+
+function search(city) {
   let apiKey = "c62ebe2801487103359e5c2553337660";
-  let city = document.querySelector("#input-city").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayWeatherCondition);
 }
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let inputCityElement = document.querySelector("#input-city");
+  search(inputCityElement.value);
+}
+
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
+form.addEventListener("submit", handleSubmit);
 
 function convertToCelsius(event) {
   event.preventDefault();
   units = "metric";
-  search(event);
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  celsius.classList.add("active");
+  fahrenheit.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 function convertToFahrenheit(event) {
   event.preventDefault();
-  units = "imperial";
-  search(event);
+  let temperatureElement = document.querySelector("#temperature");
+  celsius.classList.remove("active");
+  fahrenheit.classList.add("active");
+  let fahrenheitTemperature = Math.round(celsiusTemperature * 1.8) + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
+let celsiusTemperature = null;
 
 let celsius = document.querySelector("#celsius-link");
 celsius.addEventListener("click", convertToCelsius);
@@ -74,6 +87,4 @@ celsius.addEventListener("click", convertToCelsius);
 let fahrenheit = document.querySelector("#fahrenheit-link");
 fahrenheit.addEventListener("click", convertToFahrenheit);
 
-function changeAnimation(event) {
-  event.preventDefault();
-}
+search("Zürich");
